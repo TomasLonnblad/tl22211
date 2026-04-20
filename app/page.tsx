5b0c5a73a1941ca91b0cca57637148fd75dcf56d
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-20 08:43:13 +0200

    x

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Component3 from '@/components/shadcn-studio/button/card-17'
 import Component4 from '@/components/shadcn-studio/button/card-15'
 import Component5 from '@/components/shadcn-studio/button/button-55'
 import Component6 from '@/components/shadcn-studio/button/tooltip-11'
-import Component7 from '@/components/animated-tooltip-demo'
+//import Component7 from '@/components/animated-tooltip-demo'
 
 export default function Page() {
   return (
@@ -15,7 +15,7 @@ export default function Page() {
              <Component4 />
                 <Component5 />
                    <Component6 />
-                      <Component7 />
+                      
     </main>
 
 
