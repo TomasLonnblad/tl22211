8b10fcd35634702c9968c3e15c6ecfe960ff86d2
TomasLonnblad <tomaslonnblad@gmail.com>
2026-04-20 08:31:17 +0200

    x

---
+++ components/shadcn-studio/button/button-55.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { GlassButton } from '@/components/ui/glass-button'
+import '@/app/globals2.css'
+const GlassButtonDemo = () => {
+  return (
+    <div className='flex size-full items-center justify-center rounded-xl bg-gray-800'>
+      <GlassButton>Glass Button</GlassButton>
+    </div>
+  )
+}
+
+export default GlassButtonDemo
